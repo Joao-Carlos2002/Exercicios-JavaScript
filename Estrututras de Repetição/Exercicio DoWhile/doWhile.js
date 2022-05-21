260eda9d442b53162abf3fd70384d8e02d856bf8
Joao-Carlos2002 <jcsra2002@gmail.com>
2022-05-21 20:47:43 -0300

    main

--- Estrututras de Repetição/Exercicio DoWhile/doWhile.js
+++ Estrututras de Repetição/Exercicio DoWhile/doWhile.js
@@ -5,8 +5,7 @@ for (let i = spaceshipName.length - 1; i >= 0; i--) {
     if (spaceshipName[i] == "e") {
         break
     }
-    invertedName += spaceshipName[i
-    ];
+    invertedName += spaceshipName[i];
 };
 
 alert('Nome da nave: ' + spaceshipName + "\nNome após inversão: " + invertedName);
