4db60353fe2e9eb027699a6b08789370e9705673
Joao-Carlos2002 <jcsra2002@gmail.com>
2022-05-21 20:45:43 -0300

    main

--- Estrututras de Repetição/Exercicio DoWhile/doWhile.js
+++ Estrututras de Repetição/Exercicio DoWhile/doWhile.js
@@ -2,10 +2,10 @@ let spaceshipName = prompt("Qual o nome da nave?");
 
 let invertedName = ""
 for (let i = spaceshipName.length - 1; i >= 0; i--) {
-    if (spaceshipName[i] == 'e') {
+    if (spaceshipName[i] == "e") {
         break
     }
     invertedName += spaceshipName[1];
-}
+};
 
 alert('Nome da nave: ' + spaceshipName + "\nNome após inversão: " + invertedName);
