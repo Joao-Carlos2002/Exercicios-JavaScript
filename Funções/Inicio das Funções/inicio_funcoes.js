ee3f48546337c7ded7bf92df0d2bde031a282f60
Joao-Carlos2002 <jcsra2002@gmail.com>
2022-05-22 11:53:02 -0300

    main

---
+++ Funções/Inicio das Funções/inicio_funcoes.js
@@ -0,0 +1,12 @@
+function avisaGeral() {
+    alert('O codigo furunciou!!!!!')
+}
+
+function greetPilot(name, message = 'Olá') {
+    alert(message + ',' + name)
+}
+
+greetPilot();
+
+
+avisaGeral();
--- Funções/inicio_funcoes.js
+++
@@ -1,4 +0,0 @@
-function avisaGeral() {
-    alert('O codigo furunciou!!!!!')
-}
-avisaGeral();
